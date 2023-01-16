1fc14229ed232a22097ff788f80294a4b8784619
Veysel Perü <veyselperunak@gmail.com>
2023-01-16 12:26:26 +0300

    Commit 1

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+        giris:
+            Console.Write("Bir sayı giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 0)
+                goto giris;
+
+            if (sayi < 0)
+                Console.WriteLine("Sayı Negatif");
+            else
+                Console.WriteLine("Sayı Pozitif");
+            //Console.WriteLine("Program sona erdi.     ");
+            //Console.ReadLine();
+        }
+    }
+}
